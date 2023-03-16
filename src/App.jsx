f87be915542df37ec98113196b31af889f0ac573
jetsadawwts <jetsadawwts@gmail.com>
2023-03-16 10:12:23 +0700

    EP7 Pseudo Selector

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import EP3HeightAndWidth from "./page/EP3HeightAndWidth";
 import EP4MarginAndPadding from "./page/EP4MarginAndPadding";
 import EP5Border from "./page/EP5Border";
 import EP6BoxShadox from "./page/EP6BoxShadox";
+import EP7PseudoSelector from "./page/EP7PseudoSelector";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       {/* <EP3HeightAndWidth /> */}
       {/* <EP4MarginAndPadding /> */}
       {/* <EP5Border /> */}
-      <EP6BoxShadox />
+      {/* <EP6BoxShadox /> */}
+      <EP7PseudoSelector />
     </div>
   );
 }
